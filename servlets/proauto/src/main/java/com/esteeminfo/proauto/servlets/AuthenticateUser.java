05909ac87069e1315dee91d7daffedeb060fcbb7
Ranadheer Machineni <ranadheer.machineni@macys.com>
2016-10-03 15:10:00 +0530

    working condtion

---
+++ servlets/proauto/src/main/java/com/esteeminfo/proauto/servlets/AuthenticateUser.java
@@ -0,0 +1,53 @@
+package com.esteeminfo.proauto.servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AuthenticateUser
+ */
+@WebServlet("AuthenticateUser")
+public class AuthenticateUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AuthenticateUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+			
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userid = request.getParameter("userid");
+		String password = request.getParameter("password");
+		System.out.println(userid+" ********************************************** "+password);
+
+		request.getSession().setAttribute("userid", userid);
+		
+		doGet(request, response);
+	}
+
+}
